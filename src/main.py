0eed7c1b6df172aef24099496ad99ce9c9554ec9
EthanVIII <ethan.lee.yh@gmail.com>
2024-01-28 22:42:42 +1100

    update main.py with rot

--- src/main.py
+++ src/main.py
@@ -2,4 +2,11 @@ if __name__ == "__main__":
     main()
 
 def main():
-    
+    from codecs import encode
+    rot_number = input("ROT number")
+    try:
+        int(rot_number)
+    except:
+        print("Not a number")
+    if 
+    text_to_rot = input("Text to rot cipher")
